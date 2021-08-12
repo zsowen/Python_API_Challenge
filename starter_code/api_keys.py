43cdaa7404971d893aaf2cf20865572e25d4d1ef
zsowen <zachary.owen.10@gmail.com>
2021-08-11 21:52:09 -0400

    Work on Weather.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0fc751ece62c73ef340bb43233b647c5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyACXnxKRc1qC3liH8VzOY37i2HxFcY3dJU"
